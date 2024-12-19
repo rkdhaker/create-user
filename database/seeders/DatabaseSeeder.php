4cf9581e1ef5606aaa3b652c8dda3370610a8239
Nilesh <nilesh@softfutura.com>
2024-12-19 11:26:14 +0530

    changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,12 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'admin',
-            'email' => 'admin@admin.com',
-            'password' => Hash::make('123456'),
+       
+        // Then, insert new roles
+        Role::insert([
+            ['name' => 'customer'],
+            ['name' => 'user'],
+            ['name' => 'admin'],
         ]);
+
     }
 }
